d2fb7eb5c62d6fd7066141f82ada566904f5190b
李凌昆 <lilingkun@riryoukonnoMacBook-Pro.local>
2017-10-20 23:18:45 +0900

    fix issue

--- app/src/reduxTodos/containers/AddTodo.js
+++ app/src/reduxTodos/containers/AddTodo.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Button } from 'react-bootstrap';
-import { Form } from 'react-bootstrap';
-import { FormGroup } from 'react-bootstrap';
-import { FormControl } from 'react-bootstrap';
 import { addTodo } from '../actions'
 
 let AddTodo = ({ dispatch }) => {
@@ -11,23 +7,25 @@ let AddTodo = ({ dispatch }) => {
 
   return (
     <div>
-      <Form inline onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim()) {
-          return
-        }
-        dispatch(addTodo(input.value))
-        input.value = ''
-      }}>
-        <FormGroup>
-        <FormControl type="text" ref={node => {
-          input = node
-        }} />
-        </FormGroup>
-        <Button type="submit" bsStyle="default">
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          if (!input.value.trim()) {
+            return
+          }
+          dispatch(addTodo(input.value))
+          input.value = ''
+        }}
+      >
+        <input
+          ref={node => {
+            input = node
+          }}
+        />
+        <button type="submit">
           Add Todo
-        </Button>
-      </Form>
+        </button>
+      </form>
     </div>
   )
 }
